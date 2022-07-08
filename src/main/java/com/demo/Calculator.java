03669df2d781db9be663d20615b40c3f621fca0e
kalyanikanase <kalyanikanase@gmail.com>
2022-07-08 17:19:09 +0530

    Added Comments

--- src/main/java/com/demo/Calculator.java
+++ src/main/java/com/demo/Calculator.java
@@ -6,15 +6,22 @@ package com.demo;
  */
 public class Calculator
 {
+	//Addition
     public int add(int a, int b){
 		return a+b;
 	}
+    
+    //Substraction
     public int sub(int a, int b){
 		return a-b;
 	}
+    
+    //Multiplication
     public int mul(int a, int b){
 		return a * b;
 	}
+    
+    //Division
     public int div(int a, int b){
     	int result = 0;
     	try {
